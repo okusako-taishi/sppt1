255ec75cce64d8dc8afb51cf4bfd1930d3719728
okusako-taishi <aopotaishi@gmail.com>
2020-05-28 17:43:21 +0900

    sppt1-ユーザー編集機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
+  resources :users, only: [:edit, :update]
   resources :posts do
     resources :comments, only: :create
     resources :likes, only: :create
   end
-  resources :users, only: :show
 end
