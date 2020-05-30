f4c62da49ff2c6781580a1cf5d6305d514ec9d15
okusako-taishi <aopotaishi@gmail.com>
2020-05-30 10:55:40 +0900

    sspt1-投稿機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-  
+    @posts = Post.all
   end
 
   def new
@@ -8,6 +8,13 @@ class PostsController < ApplicationController
   end
 
   def create
+    Post.create(post_params)
+    redirect_to root_path
+  end
 
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :link,).merge(user_id: current_user.id)
   end
 end
