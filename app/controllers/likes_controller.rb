3d2931be0381285777a1d89efee659364dc2052c
okusako-taishi <aopotaishi@gmail.com>
2020-07-01 13:53:32 +0900

    いいね機能ajax化実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,17 +1,18 @@
 class LikesController < ApplicationController
+  before_action :set_post
 
   def create
     @like = current_user.likes.create(post_id: params[:post_id])
-    if @like.save
-      redirect_back(fallback_location: root_path)
-    else
-      redirect_to root_path
-    end
+    @like.save
   end
 
   def destroy
     @like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
     @like.destroy
-    redirect_back(fallback_location: root_path)
+  end
+
+  private
+  def set_post
+    @post = Post.find(params[:post_id])
   end
 end
