16180f32ca97f9fffb4d1d742eadeeb137dcbf0a
okusako-taishi <aopotaishi@gmail.com>
2020-07-14 15:37:37 +0900

    非同期通信実装

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,23 +1,26 @@
 $(function(){
+  var list = document.getElementsByClassName("content-title");
+  var list2 = document.getElementsByClassName("content-title--sub")
+  var listContents = $(list).clone();
+  var list2Contents = $(list2).clone();
+  // var listDelete = $(list).remove();
+  // var list2Delete = $(list2).remove();
   $("#favorite-post").on("click", function(){
-    var list = document.getElementsByClassName("content-title");
-    for (let i = 0; i < list.length; i = i + 1) {
-      $(".my__main-post2").append(list[i]);
+    for (let i = 0; i < listContents.length; i = i + 1) {
+      $(".my__main-post").append(listContents[i]);
     }
-    console.log(list[0]);
-    // $.ajax({
-    //   type: "GET",
-    //   url: "/show.json",
-    //   data: {favor},
-    //   dataType: "json"
-    // })
-    // .done(function(data){
-    //   $(`<div class="favorite-post-wrapper__added">`).append(data);
-    // })
-    // .fail(function(){
-    //   alert("error");
-    // });
+    list2Contents.remove();
   });
+
+  $("#my-post").on("click", function(){
+    for (let i = 0; i < list2Contents.length; i = i + 1) {
+      $(".my__main-post").append(list2Contents[i]);
+    }
+    listContents.remove();
+  });
+
+
 });
+
 
 
