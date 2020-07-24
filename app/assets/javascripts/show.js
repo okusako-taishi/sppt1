90a0d3a8eac1fa27546a1fb05c0330b4c617e83d
okusako-taishi <aopotaishi@gmail.com>
2020-07-24 10:31:22 +0900

    ユーザーマイページ 以外でのjsの発動の修正

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,6 +1,6 @@
 $(function(){
-  var list = document.getElementsByClassName("content-title");
-  var list2 = document.getElementsByClassName("content-title--sub");
+  var list = document.getElementsByClassName("favorite--post");
+  var list2 = document.getElementsByClassName("my--post");
   $(list).hide();
   $(list2).hide();
   $("#favorite-post").on("click", function(){
